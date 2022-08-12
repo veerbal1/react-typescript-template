b45c990515ae566bc13953fcd13b87982e504bcb
veerbal1 <veerbal1singh1@gmail.com>
2022-08-12 11:53:59 +0530

    Added Test Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
 import './styles.css';
 
 const App = () => {
   return (
     <>
-      <h1>App {process.env.name} Singh</h1>
+      <h1>App {process.env.name}</h1>
+      <TestComponent text='Test Component' />
     </>
   );
+};
+
+type TestComponentProps = {
+  text: string;
+};
+
+const TestComponent = ({ text }: TestComponentProps) => {
+  return <h1>{text}</h1>;
 };
 
 export default App;
